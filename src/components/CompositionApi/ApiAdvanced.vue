e028c58b8804b8abe69525e543b1aab91edc3d49
gxuud <igaoxian@foxmail.com>
2022-08-22 16:42:58 +0800

    chore: use api advanced

--- src/components/CompositionApi/ApiAdvanced.vue
+++ src/components/CompositionApi/ApiAdvanced.vue
@@ -3,14 +3,50 @@
     {{ JSON.stringify(sref, null, 2) }}
     -{{ udref }}-
     <br>
+    <div class="block">
+      <div>{{ JSON.stringify(sReactive, null, 2) }}</div>
+      <div>{{ JSON.stringify(controlGroupSReactive, null, 2) }}</div>
+    </div>
+    <h6>readonly</h6>
+    <div class="block">
+      <!-- <h1>readonly</h1> -->
+      <div>{{ JSON.stringify(sReadonly, null, 2) }}</div>
+      <div>{{ JSON.stringify(controlGroupSReadonly, null, 2) }}</div>
+    </div>
     <button @click="() => click('click')">click</button>
     <button @click="() => click('trigger')">trigger</button>
     <button @click="() => click('d')">Debounce</button>
+    <button @click="() => click('shallowReactive')">shallowReactive</button>
+    <button @click="() => click('controlGroupReactive')">controlGroupReactive</button>
+    <button @click="() => click('shallowReadonly')">shallowReadonly</button>
+    <button @click="() => click('controlGroupReadonly')">controlGroupReadonly</button>
+    <button @click="() => click('toRaw')">toRaw</button>
+    <button @click="() => click('markRaw')">markRaw</button>
+    <button @click="() => click('counter')">counter</button>
+    <button @click="() => click('getCurrentScope')">getCurrentScope</button>
   </div>
 </template>
 
 <script lang="ts">
-import { shallowRef, triggerRef, customRef } from 'vue';
+import { 
+  ref,
+  shallowRef,
+  triggerRef,
+  customRef,
+  shallowReactive,
+  reactive,
+  shallowReadonly,
+  readonly,
+  toRaw,
+  markRaw,
+  isReactive,
+  effectScope,
+  computed,
+  watch,
+  watchEffect,
+  getCurrentScope,
+  onScopeDispose,
+} from 'vue';
 
 function useDebouncedRef(value, delay = 200) {
   let timeout
@@ -33,7 +69,27 @@ function useDebouncedRef(value, delay = 200) {
 export default {
   setup() {
     const sref = shallowRef({ bbq: 'bbq' });
+    // customRef
     const udref = useDebouncedRef(0);
+    const sReactive = reactive({ bbq: { bbq996: 0 } });
+    const controlGroupSReactive = shallowReactive({ bbq: { bbq996: 0 } });
+    const sReadonly = readonly({ bbq: { bbq996: 0 } });
+    const controlGroupSReadonly = shallowReadonly({ bbq: { bbq996: 0 } });
+    const raw1 = reactive({ bbq: 'bbq' });
+    const raw2 = shallowReactive({ bbq: 'bbq' });
+    const raw3 = readonly({ bbq: 'bbq' });
+    const raw4 = shallowReadonly({ bbq: 'bbq' });
+    const scope = effectScope();
+    const counter = ref(1)
+
+    scope.run(() => {
+      const doubled = computed(() => counter.value * 2)
+      watchEffect(() => console.log('Count: ', doubled.value))
+    })
+    const gcs = getCurrentScope();
+    onScopeDispose(() => {
+      console.log('hello world')
+    });
     const click = (param) => {
       if (param === 'click') {
         sref.value.bbq += '-bbq'
@@ -41,9 +97,39 @@ export default {
         triggerRef(sref);
       } else if (param === 'd') {
         udref.value ++;
+      } else if (param === 'shallowReactive') {
+        sReactive.bbq.bbq996 ++;
+      } else if (param === 'controlGroupReactive') {
+        controlGroupSReactive.bbq.bbq996 ++;
+      } else if (param === 'shallowReadonly') {
+        sReadonly.bbq.bbq996 ++;
+      } else if (param === 'controlGroupshallowReadonly') {
+        controlGroupSReadonly.bbq.bbq996 ++;
+      } else if (param === 'toRaw') {
+        console.log(raw1, raw2, raw3, raw4);
+        console.log(toRaw(raw1), toRaw(raw2), toRaw(raw3), toRaw(raw4));
+      } else if (param === 'markRaw') {
+        const foo = markRaw({})
+        console.log(isReactive(reactive(foo))) // false
+        // 也适用于嵌套在其他响应性对象
+        const bar = reactive({ foo })
+        console.log(isReactive(bar.foo)) // false
+      } else if (param === 'counter') {
+        counter.value ++;
+      } else if (param === 'getCurrentScope') {
+        console.log(gcs);
       }
     }
-    return { sref, click, udref };
+    return { sref, click, udref, sReactive, controlGroupSReactive, sReadonly, controlGroupSReadonly };
   }
 }
 </script>
+
+<style>
+.block {
+  display: flex;
+  width: 1000px;
+  justify-content: space-between;
+  margin: 0 auto;
+}
+</style>
