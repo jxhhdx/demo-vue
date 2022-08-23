666bbbff156f61de626134ba5365165332f8bd82
gxuud <igaoxian@foxmail.com>
2022-08-23 14:39:45 +0800

    chore: use css module

---
+++ src/components/SingleFileComponent/UseCssFeatures/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="example">hi</div>
+  <!-- <p :class="$style.red">This should be red</p> -->
+  <p :class="bbq996.red">This should be red</p>
+</template>
+
+<script setup>
+
+</script>
+
+<!-- <style scoped> -->
+<!-- <style module> -->
+<style module="bbq996">
+.example {
+  color: red;
+}
+.red {
+  color: red;
+}
+</style>
--- src/components/SingleFileComponent/index.vue
+++ src/components/SingleFileComponent/index.vue
@@ -1,13 +1,15 @@
 <template>
   <syntax-specifition />
   <use-setup />
+  <use-css-features />
 </template>
 
 <script lang="ts">
 import SyntaxSpecifition from './SyntaxSpecifition/index.vue';
 import UseSetup from './UseSetup/index.vue';
+import UseCssFeatures from './UseCssFeatures/index.vue';
 
 export default {
-  components: { SyntaxSpecifition, UseSetup }
+  components: { SyntaxSpecifition, UseSetup, UseCssFeatures }
 }
 </script>
