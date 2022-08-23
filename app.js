c4180730359e17096daec66a05fa0269bdf520f2
gxuud <igaoxian@foxmail.com>
2022-08-23 16:59:05 +0800

    chore: use basic ssr

---
+++ app.js
@@ -0,0 +1,7 @@
+import { createSSRApp, h, ref } from 'vue'
+export function createApp() {
+  return createSSRApp({
+    data: () => ({ count: 1 }),
+    template: `<div @click="count++">{{ count }}</div>`,
+  });
+}
